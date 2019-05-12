a320694f776c4cb7fbe426ba84daa6b439a23c01
Aaron Yang <ycr_aaron@foxmail.com>
2019-05-12 20:30:40 +0800

    mongoose pagination ts

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+import {
+  IPagination,
+  IPaginateResult,
+  IPaginateOptions,
+  IPaginateDefaultOptions,
+  Schema,
+  PaginateModel,
+  DocumentQuery
+} from 'mongoose';
+const defaultOptions: IPaginateDefaultOptions = {
+  lean: false,
+  perPage: 10,
+  projection: '',
+  select: ''
+};
+
+function genMongooseQuery(
+  model: PaginateModel<any>,
+  conditions: object,
+  options: IPaginateOptions & IPaginateDefaultOptions
+): DocumentQuery<any, any> {
+  let {
+    collation,
+    lean,
+    perPage,
+    populate,
+    projection,
+    select,
+    sort,
+    page
+  } = options;
+
+  const query = model
+    .find(conditions, projection)
+    .select(select)
+    .sort(sort)
+    .lean(lean);
+  if (typeof collation !== 'undefined') {
+    if (Object.keys(collation).length > 0) {
+      query.collation(collation);
+    }
+  }
+
+  if (typeof populate !== 'undefined') {
+    query.populate(populate);
+  }
+  // page === 'undefined' <=> no pagination
+  if (typeof page !== 'undefined') {
+    let skip = (page - 1) * (perPage as number);
+    query.skip(skip).limit(perPage);
+  }
+  return query;
+}
+
+function genPagination(
+  options: IPaginateOptions & IPaginateDefaultOptions,
+  count: number
+) {
+  let { perPage, page } = options;
+  const pagination: IPagination = {
+    hasPrevPage: false,
+    hasNextPage: false,
+    prevPage: null,
+    nextPage: null,
+    perPage: perPage
+  };
+  if (typeof page !== 'undefined') {
+    const totalPages = Math.ceil(count / perPage) || 1;
+    pagination.totalPages = totalPages;
+    pagination.page = page;
+
+    if (page > totalPages) {
+      page = totalPages;
+    }
+    if (page > 1) {
+      pagination.hasPrevPage = true;
+      pagination.prevPage = page - 1;
+    }
+    if (page < totalPages) {
+      pagination.hasNextPage = true;
+      pagination.nextPage = page + 1;
+    }
+  }
+  return pagination;
+}
+export async function paginate(
+  this: PaginateModel<any>,
+  conditions: object,
+  options: IPaginateOptions & IPaginateDefaultOptions,
+  callback: Function
+) {
+  const isCallbackSpecified = typeof callback === 'function';
+
+  try {
+    options = {
+      ...defaultOptions,
+      ...options
+    };
+
+    conditions = conditions || {};
+    const mongooseQuery = genMongooseQuery(this, conditions, options);
+    const docs = await mongooseQuery.exec();
+    const count = await this.countDocuments(conditions).exec();
+    const result: IPaginateResult<any> = {
+      data: docs,
+      pagination: genPagination(options, count)
+    };
+
+    return isCallbackSpecified ? callback(null, result) : result;
+  } catch (err) {
+    return isCallbackSpecified ? callback(err) : err;
+  }
+}
+
+export function mongoosePagination(schema: Schema) {
+  schema.statics.paginate = paginate;
+}
---
+++ src/types.ts
@@ -0,0 +1,52 @@
+declare module 'mongoose' {
+  interface IPaginateOptions {
+    select?: Object | string;
+    sort?: Object | string;
+    collation?: CollationOptions;
+    projection?: Object | string;
+    populate?: Object[] | string[] | Object | string;
+    lean?: boolean;
+    page?: number;
+    perPage?: number;
+  }
+  interface IPaginateDefaultOptions {
+    select: string;
+    projection: string;
+    lean: boolean;
+    perPage: number;
+  }
+
+  interface IPaginateResult<T> {
+    data: T[];
+    pagination: IPagination;
+  }
+
+  interface IPagination {
+    hasPrevPage: boolean;
+    hasNextPage: boolean;
+    prevPage: number | null;
+    nextPage: number | null;
+    perPage: number;
+    page?: number | null;
+    totalPages?: number;
+  }
+
+  interface PaginateModel<T extends Document> extends Model<T> {
+    paginate(
+      query?: object,
+      options?: IPaginateOptions,
+      callback?: (err: any, result: IPaginateResult<T>) => void
+    ): Promise<IPaginateResult<T>>;
+  }
+
+  function model(
+    name: string,
+    schema?: Schema,
+    collection?: string,
+    skipInit?: boolean
+  ): PaginateModel<any>;
+}
+
+import mongoose = require('mongoose');
+declare function _(schema: mongoose.Schema): void;
+export = _;
