3739c169aa1c009c7fdcdd5e938384a7407d2af1
Aaron Yang <ycr_aaron@foxmail.com>
2019-05-14 17:23:08 +0800

    test cases

--- src/tests/Pagination.test.ts
+++ src/tests/Pagination.test.ts
@@ -1,43 +1,70 @@
 import mongoose from "mongoose";
-import { Schema, Document, PaginateModel } from "mongoose";
-import { mongoosePagination } from "..";
-const ObjectId = Schema.Types.ObjectId;
+import { User, Account, TUser } from "./fixtures";
 
-// export type
-type TUser = IUser & Document;
-interface IUser {
-  _id: string;
-  name: string;
-  accounts: (string | TAccount)[];
-}
-
-type TAccount = IAccount & Document;
-interface IAccount {
-  _id: string;
-  name: string;
-  user: string;
-}
-const userSchema = new Schema(
+const dummmUserName = "Ann0316";
+const dummyAccountName1 = "dummyAccount1";
+const dummyAccountName2 = "dummyAccount2";
+const dummyAccountName3 = "dummyAccount3";
+const dummyAccountName4 = "dummyAccount4";
+const dummyAccountName5 = "dummyAccount5";
+const dummyAccountName6 = "dummyAccount6";
+const dummyAccountName7 = "dummyAccount7";
+const dummyAccountName8 = "dummyAccount8";
+const dummyAccountName9 = "dummyAccount9";
+const dummyAccountName10 = "dummyAccount10";
+const dummyAccountName11 = "dummyAccount11";
+const testUser = { username: dummmUserName };
+const testAccounts = [
   {
-    name: String,
-    accounts: [{ type: ObjectId, ref: "Account" }]
+    accountName: dummyAccountName1
   },
-  { timestamps: true }
-);
-userSchema.plugin(mongoosePagination);
+  {
+    accountName: dummyAccountName2
+  },
+  {
+    accountName: dummyAccountName3
+  },
+  {
+    accountName: dummyAccountName4
+  },
+  {
+    accountName: dummyAccountName5
+  },
+  {
+    accountName: dummyAccountName6
+  },
+  {
+    accountName: dummyAccountName7
+  },
+  {
+    accountName: dummyAccountName8
+  },
+  {
+    accountName: dummyAccountName9
+  },
+  {
+    accountName: dummyAccountName10
+  },
+  {
+    accountName: dummyAccountName11
+  }
+];
 
-const User: PaginateModel<TUser> = mongoose.model("User", userSchema);
-function isTUser(obj: any): obj is TUser {
-  return obj && obj._id;
-}
-
-const testUser = { name: "test" };
 async function prepareData() {
   await User.create(testUser);
+  await Account.create(testAccounts);
+  const account = await Account.findOne({ accountName: dummyAccountName1 });
+  const user = await User.findOne();
+  user.accounts.push(account._id);
+  account.user = user._id;
+  await user.save();
+  await account.save();
 }
 async function clearData() {
   await User.collection.drop();
+  await Account.collection.drop();
 }
+
 beforeAll(async () => {
   mongoose.connect("mongodb://localhost:27017/pagination-test", {
     useNewUrlParser: true
@@ -45,13 +72,82 @@ beforeAll(async () => {
   await prepareData();
 });
 afterAll(async done => {
+  await clearData();
   mongoose.disconnect(done);
-  await clearData;
 });
 
-test("paginate", async done => {
-  const result = await User.paginate({});
-  console.log(result);
-  expect(result.data.length).toBe(1);
+test("paginate with page undefined", async done => {
+  // no pagination, return all docs
+  const accounts = await Account.paginate({});
+  expect(accounts.data.length).toBe(11);
+  expect(accounts.pagination).toEqual({
+    hasPrevPage: false,
+    hasNextPage: false,
+    prevPage: null,
+    nextPage: null,
+    perPage: 10
+  });
+  done();
+});
+
+test("paginate with page specified and default perPage", async done => {
+  // 10 perpage
+  const accounts = await Account.paginate({}, { page: 1 });
+  expect(accounts.data.length).toBe(10);
+  done();
+});
+
+test("paginate with page specified and customized perPage", async done => {
+  // 5 perPage
+  const accounts = await Account.paginate({}, { page: 1, perPage: 5 });
+  expect(accounts.data.length).toBe(5);
+  done();
+});
+
+test("paginate with page more than actual pages", async done => {
+  // 11 docs, 5 perpage, 1 doc in last page
+  const accounts = await Account.paginate({}, { page: 100, perPage: 5 });
+  expect(accounts.data.length).toBe(1);
+  done();
+});
+
+test("paginate with previous page and next page", async done => {
+  // 10 perpage
+  const accounts = await Account.paginate({}, { page: 2, perPage: 5 });
+  expect(accounts.data.length).toBe(5);
+  expect(accounts.pagination).toEqual({
+    hasPrevPage: true,
+    hasNextPage: true,
+    prevPage: 1,
+    nextPage: 3,
+    perPage: 5,
+    page: 2,
+    totalPages: 3
+  });
+  done();
+});
+
+test("paginate with populate", async done => {
+  const accounts = await Account.paginate(
+    {},
+    { page: 1, perPage: 5, populate: "user" }
+  );
+  expect(accounts.data.length).toBe(5);
+  expect((accounts.data[0].user as TUser).username).toBe(dummmUserName);
+  done();
+});
+
+test("paginate with collation", async done => {
+  const accounts = await Account.paginate(
+    {},
+    {
+      page: 1,
+      perPage: 5,
+      populate: "user",
+      collation: { locale: "en_US", strength: 1 }
+    }
+  );
+  expect(accounts.data.length).toBe(5);
+  expect((accounts.data[0].user as TUser).username).toBe(dummmUserName);
   done();
 });
---
+++ src/tests/fixtures.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { Schema, Document, PaginateModel } from "mongoose";
+import { mongoosePagination } from "..";
+const ObjectId = Schema.Types.ObjectId;
+
+export type TUser = IUser & Document;
+interface IUser {
+  _id: string;
+  username: string;
+  accounts: (string | TAccount)[];
+}
+
+export type TAccount = IAccount & Document;
+interface IAccount {
+  _id: string;
+  accountName: string;
+  user: string | TUser;
+}
+
+const userSchema = new Schema({
+  username: String,
+  accounts: [{ type: ObjectId, ref: "Account" }]
+});
+const accountSchema = new Schema({
+  accountName: String,
+  user: { type: ObjectId, ref: "User" }
+});
+
+userSchema.plugin(mongoosePagination);
+accountSchema.plugin(mongoosePagination);
+
+export const User: PaginateModel<TUser> = mongoose.model("User", userSchema);
+export const Account: PaginateModel<TAccount> = mongoose.model(
+  "Account",
+  accountSchema
+);
+export function isTUser(obj: any): obj is TUser {
+  return obj && obj._id;
+}
